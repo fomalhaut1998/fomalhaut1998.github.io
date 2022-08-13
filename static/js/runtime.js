2ac740bb8bd64d4b8daea150cf274186a23f1a66
fomalhaut1998 <1174008660@qq.com>
2022-08-13 22:59:59 +0800

    Site updated: 2022-08-13 22:59:57

---
+++ static/js/runtime.js
@@ -0,0 +1,36 @@
+setInterval(() => {
+  let create_time = Math.round(new Date('2022-08-09 00:00:00').getTime() / 1000); //在此行修改建站时间
+  let timestamp = Math.round((new Date().getTime()) / 1000);
+  let second = timestamp - create_time;
+  let time = new Array(0, 0, 0, 0, 0);
+
+  var nol = function(h){
+    return h>9?h:'0'+h;
+  }
+  if (second >= 365 * 24 * 3600) {
+    time[0] = parseInt(second / (365 * 24 * 3600));
+    second %= 365 * 24 * 3600;
+  }
+  if (second >= 24 * 3600) {
+    time[1] = parseInt(second / (24 * 3600));
+    second %= 24 * 3600;
+  }
+  if (second >= 3600) {
+    time[2] = nol(parseInt(second / 3600));
+    second %= 3600;
+  }
+  if (second >= 60) {
+    time[3] = nol(parseInt(second / 60));
+    second %= 60;
+  }
+  if (second > 0) {
+    time[4] = nol(second);
+  }
+  if ((Number(time[2])<22) && (Number(time[2])>7)){
+    currentTimeHtml ="<img class='boardsign' src='https://img.shields.io/badge/F小屋-营业中-6adea8?style=social&logo=cakephp' title='距离百年老店也就差不到一百年~'><div id='runtime'>" + time[0] + ' YEAR ' + time[1] + ' DAYS ' + time[2] + ' : ' + time[3] + ' : ' + time[4] + '</div>';
+  }
+  else{
+    currentTimeHtml ="<img class='boardsign' src='https://img.shields.io/badge/F小屋-打烊了-6adea8?style=social&logo=coffeescript' title='这个点了应该去睡觉啦，熬夜对身体不好哦'><div id='runtime'>" + time[0] + ' YEAR ' + time[1] + ' DAYS ' + time[2] + ' : ' + time[3] + ' : ' + time[4] + '</div>';
+  }
+  document.getElementById("workboard").innerHTML = currentTimeHtml;
+}, 1000);
